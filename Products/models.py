902f45d5ea7737a125a577376fb3216a292cb096
Mr-Ndi <onlythenotes@gmail.com>
2024-12-18 09:40:48 +0200

    Other adjustment about product model

--- Products/models.py
+++ Products/models.py
@@ -3,11 +3,11 @@ from Sellers.models import Sellers
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    name = models.ChatField(max_length=20)
-    price = models.decimalField()
-    stock_quantity = models.integerField()
-    unit = models.stringField(min_length=2, max_length=3)
-    minimum_for_deliver = models.integerField()
+    name = models.CharField(max_length=20)
+    price = models.DecimalField()
+    stock_quantity = models.IntegerField()
+    unit = models.CharField(max_length=3)
+    minimum_for_deliver = models.IntegerField()
     description = models.CharField(min_length=10, max_length=200)
     owner_id = models.ForeignKey(sellers, on_delete=models.CASCADE)
 
