5bf2ba990aafd9ff4cca088761e02c32a3c36b60
Mr-Ndi <onlythenotes@gmail.com>
2024-12-18 09:49:09 +0200

    pushed product models

--- Products/models.py
+++ Products/models.py
@@ -4,12 +4,12 @@ from Sellers.models import Sellers
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
-    price = models.DecimalField()
+    price = models.IntegerField()
     stock_quantity = models.IntegerField()
     unit = models.CharField(max_length=3)
     minimum_for_deliver = models.IntegerField()
-    description = models.CharField(min_length=10, max_length=200)
-    owner_id = models.ForeignKey(sellers, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    owner_id = models.ForeignKey(Sellers, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
