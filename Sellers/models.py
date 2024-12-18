e0d0b317c9d91b1f0e994534097476a029ac1532
Mr-Ndi <onlythenotes@gmail.com>
2024-12-18 08:19:02 +0200

    after correcting seller model

--- Sellers/models.py
+++ Sellers/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Sellers(models.Model):
     seller_id = models.IntegerField(primary_key=True)
     names = models.CharField(max_length=32)
-    email = model.EmailField()
+    email = models.EmailField()
     password = models.CharField(max_length=32)
     district = models.CharField(max_length=32)
     sector = models.CharField(max_length=32)
